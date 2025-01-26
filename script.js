ba317c5c99ccbf35578f797fecb748b2c10dab09
WinXprt <nutella.is.better.than.merenda@gmail.com>
2025-01-26 12:14:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function PlayPause() {
+if (document.querySelector(".audio").paused) {
+document.querySelector("audio").play();
+document.querySelector(".play-pause-btn").src = 'https://www.iconsdb.com/icons/preview/green/pause-xxl.png'
+document.querySelector(".play-pause-btn").style = "position: fixed; margin-top: 38vh; margin-left: 89vw; animation: fade-in-out 2.5s linear infinite;";
+document.querySelector(".stop-btn").style = "position: fixed; margin-top: 50.5vh; margin-left: 89vw; animation: fade-in-out 2.5s linear infinite;";
+} else {
+document.querySelector("audio").pause();
+document.querySelector(".play-pause-btn").src = 'https://www.iconsdb.com/icons/preview/green/play-xxl.png'
+document.querySelector(".play-pause-btn").style = "position: fixed; margin-top: 38vh; margin-left: 89vw;";
+document.querySelector(".stop-btn").style = "position: fixed; margin-top: 50.5vh; margin-left: 89vw;"
+}
+}
+function StopAudio() {
+document.querySelector("audio").pause();
+document.querySelector("audio").currentTime = 0;
+document.querySelector(".play-pause-btn").src = 'https://www.iconsdb.com/icons/preview/green/play-xxl.png'
+document.querySelector(".play-pause-btn").style = "position: fixed; margin-top: 38vh; margin-left: 89vw;";
+document.querySelector(".stop-btn").style = "position: fixed; margin-top: 50.5vh; margin-left: 89vw;"
+}
