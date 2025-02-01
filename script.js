46d4ee94120fe4dd5b0fa6d9861e017b289f7d18
WinXprt <nutella.is.better.than.merenda@gmail.com>
2025-02-01 21:54:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function PlayPause() {
 if (document.querySelector(".audio").paused) {
 document.querySelector("audio").play();
 document.querySelector(".play-pause-btn").src = 'pause-xxl.png'
-document.querySelector(".play-pause-btn").style = "position: fixed; margin-top: 38vh; margin-left: 89vw; animation: fade-in-out 2.5s linear infinite;";
+document.querySelector(".play-pause-btn").style = "z-index: 15; position: fixed; margin-top: 38vh; margin-left: 89vw; animation: fade-in-out 2.5s linear infinite;";
 document.querySelector(".play-pause-btn").title = "Pause music";
 document.querySelector(".stop-btn").style = "position: fixed; margin-top: 50.5vh; margin-left: 89vw; animation: fade-in-out 2.5s linear infinite;";
 } else {
 document.querySelector("audio").pause();
 document.querySelector(".play-pause-btn").src = 'play-xxl.png'
-document.querySelector(".play-pause-btn").style = "position: fixed; margin-top: 38vh; margin-left: 89vw;";
+document.querySelector(".play-pause-btn").style = "z-index: 15; position: fixed; margin-top: 38vh; margin-left: 89vw;";
 document.querySelector(".play-pause-btn").title = "Play music";
 document.querySelector(".stop-btn").style = "position: fixed; margin-top: 50.5vh; margin-left: 89vw;"
 }
@@ -17,7 +17,7 @@ function StopAudio() {
 document.querySelector("audio").pause();
 document.querySelector("audio").currentTime = 0;
 document.querySelector(".play-pause-btn").src = 'play-xxl.png'
-document.querySelector(".play-pause-btn").style = "position: fixed; margin-top: 38vh; margin-left: 89vw;";
+document.querySelector(".play-pause-btn").style = "z-index: 15; position: fixed; margin-top: 38vh; margin-left: 89vw;";
 document.querySelector(".stop-btn").style = "position: fixed; margin-top: 50.5vh; margin-left: 89vw;"
 }
 function BlueTheme() {
@@ -72,50 +72,81 @@ document.write(`
 }
 document.querySelector("audio").onplay = function() {
 document.querySelector(".play-pause-btn").src = 'pause-xxl.png'
-document.querySelector(".play-pause-btn").style = "position: fixed; margin-top: 38vh; margin-left: 89vw; animation: fade-in-out 2.5s linear infinite;";
+document.querySelector(".play-pause-btn").style = "z-index: 15; position: fixed; margin-top: 38vh; margin-left: 89vw; animation: fade-in-out 2.5s linear infinite;";
 document.querySelector(".play-pause-btn").title = "Pause music";
 document.querySelector(".stop-btn").style = "position: fixed; margin-top: 50.5vh; margin-left: 89vw; animation: fade-in-out 2.5s linear infinite;";
-document.querySelector(".next-track").style = "margin-left: 35px; display: inline-block; position: fixed; margin-top: 50vh; cursor: pointer; animation: fade-in-out 2.5s linear infinite;";
-document.querySelector(".prev-track").style = "transform: scaleX(-1); margin-left: 35px; display: inline-block; position: fixed; margin-top: 62vh; cursor: pointer; animation: fade-in-out 2.5s linear infinite;";
+document.querySelector(".next-track").style = "margin-left: 15px; display: inline-block; position: fixed; margin-top: 50vh; cursor: pointer; animation: fade-in-out 2.5s linear infinite;";
+document.querySelector(".prev-track").style = "transform: scaleX(-1); margin-left: 15px; display: inline-block; position: fixed; margin-top: 62vh; cursor: pointer; animation: fade-in-out 2.5s linear infinite;";
 };
 document.querySelector("audio").onpause = function () {
 document.querySelector(".play-pause-btn").src = 'play-xxl.png'
-document.querySelector(".play-pause-btn").style = "position: fixed; margin-top: 38vh; margin-left: 89vw;";
+document.querySelector(".play-pause-btn").style = "z-index: 15; position: fixed; margin-top: 38vh; margin-left: 89vw;";
 document.querySelector(".play-pause-btn").title = "Play music";
 document.querySelector(".stop-btn").style = "position: fixed; margin-top: 50.5vh; margin-left: 89vw;"
-document.querySelector(".next-track").style = "margin-left: 35px; display: inline-block; position: fixed; margin-top: 50vh; cursor: pointer;";
-document.querySelector(".prev-track").style = "transform: scaleX(-1); margin-left: 35px; display: inline-block; position: fixed; margin-top: 62vh; cursor: pointer;";
+document.querySelector(".next-track").style = "margin-left: 15px; display: inline-block; position: fixed; margin-top: 50vh; cursor: pointer;";
+document.querySelector(".prev-track").style = "transform: scaleX(-1); margin-left: 15px; display: inline-block; position: fixed; margin-top: 62vh; cursor: pointer;";
 };
 var CurrentSong;
-const SongArray = ["Coldplay - In My Place.mp3", "Coldplay - Yellow.mp3", "Green Day - Boulevard of Broken Dreams.mp3", "Green Day - When I Come Around.mp3"];
+const SongArray = ["Coldplay - The Scientist", "Coldplay - In My Place", "Coldplay - Yellow", "Green Day - Boulevard of Broken Dreams", "Green Day - When I Come Around", "Green Day - Good Riddance", "blink-182 - Adam's Song", "blink-182 - I Miss You", "blink-182 - Story Of A Lonely Guy"];
 function ShuffleSong() {
-CurrentSongNum = Math.round(Math.random() * 3);
-document.querySelector(".audio").src = SongArray[CurrentSongNum];
+CurrentSongNum = Math.round(Math.random() * 8);
+document.querySelector(".audio").src = SongArray[CurrentSongNum] + ".mp3";
+document.querySelector(".music-info").innerHTML = `
+<span>♫:</span> 
+<span>${SongArray[CurrentSongNum].split(" - ")[0]}</span>
+<hr style="margin: 0;">
+<span>${SongArray[CurrentSongNum].split(" - ")[1]}</span>
+`;
 }
 ShuffleSong();
 function TrackChange(value) {
 if (value == 1) {
-if (CurrentSongNum == 3) {
+if (CurrentSongNum == 8) {
 CurrentSongNum = 0;
-document.querySelector("audio").src = SongArray[CurrentSongNum];
+document.querySelector("audio").src = SongArray[CurrentSongNum] + ".mp3";
 document.querySelector("audio").play();
+document.querySelector(".music-info").innerHTML = `
+<span>♫:</span> 
+<span>${SongArray[CurrentSongNum].split(" - ")[0]}</span>
+<hr style="margin: 0;">
+<span>${SongArray[CurrentSongNum].split(" - ")[1]}</span>
+`;
 return;
 }
 CurrentSongNum += 1;
-document.querySelector("audio").src = SongArray[CurrentSongNum];
+document.querySelector("audio").src = SongArray[CurrentSongNum] + ".mp3";
 document.querySelector("audio").play();
+document.querySelector(".music-info").innerHTML = `
+<span>♫:</span> 
+<span>${SongArray[CurrentSongNum].split(" - ")[0]}</span>
+<hr style="margin: 0;">
+<span>${SongArray[CurrentSongNum].split(" - ")[1]}</span>
+`;
 } else if (value == -1) {
 if (CurrentSongNum == 0) {
-CurrentSongNum = 3;
-document.querySelector("audio").src = SongArray[CurrentSongNum];
+CurrentSongNum = 8;
+document.querySelector("audio").src = SongArray[CurrentSongNum] + ".mp3";
 document.querySelector("audio").play();
+document.querySelector(".music-info").innerHTML = `
+<span>♫:</span> 
+<span>${SongArray[CurrentSongNum].split(" - ")[0]}</span>
+<hr style="margin: 0;">
+<span>${SongArray[CurrentSongNum].split(" - ")[1]}</span>
+`;
 return;
 }
 CurrentSongNum -= 1;
-document.querySelector("audio").src = SongArray[CurrentSongNum];
+document.querySelector("audio").src = SongArray[CurrentSongNum] + ".mp3";
 document.querySelector("audio").play();
+document.querySelector(".music-info").innerHTML = `
+<span>♫:</span> 
+<span>${SongArray[CurrentSongNum].split(" - ")[0]}</span>
+<hr style="margin: 0;">
+<span>${SongArray[CurrentSongNum].split(" - ")[1]}</span>
+`;
 }
 }
+
 //ONLY ON INDEX.HTML. CODE ABOVE
 function YtIframeCookies(value) {
 if (value == '0') {
